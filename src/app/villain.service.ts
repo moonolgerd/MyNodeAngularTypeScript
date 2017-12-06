dc47ab77eedf932ce99f80586be3b664d5022257
Oleg <moonolgerd@gmail.com>
2017-12-06 00:16:22 -0500

    Removed semicolons (again)

--- src/app/villain.service.ts
+++ src/app/villain.service.ts
@@ -1,47 +1,46 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs/Rx'
 
-import 'rxjs/add/operator/toPromise';
-import { IGenericService } from './generic.service';
-import { Villain } from './villain';
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/toPromise'
+import { IGenericService } from './generic.service'
+import { Villain } from './villain'
+import { HttpClient } from '@angular/common/http'
+import { HttpHeaders } from '@angular/common/http'
 
 @Injectable()
 export class VillainService implements IGenericService<Villain> {
 
-    private headers = new Headers({ 'Content-Type': 'application/json' });
-    private villainsUrl = 'http://localhost:5000/api/villains';  // URL to web api
+    private headers = new Headers({ 'Content-Type': 'application/json' })
+    private villainsUrl = 'http://localhost:5000/api/villains'  // URL to web api
 
     private static handleError(error: any): Promise<Villain> {
-        console.error('An error occurred', error); // for demo purposes only
-        return Promise.reject(error.message || error);
+        console.error('An error occurred', error) // for demo purposes only
+        return Promise.reject(error.message || error)
     }
 
     constructor(private http: HttpClient) { }
 
     async get(): Promise<Villain[]> {
-        return await this.http.get<Villain[]>(this.villainsUrl).toPromise();
+        return await this.http.get<Villain[]>(this.villainsUrl).toPromise()
     }
 
     delete(id: number) {
-        const myString = `${this.villainsUrl}/${id.toString()}`;
+        const myString = `${this.villainsUrl}/${id.toString()}`
         return this.http.delete<Villain>(myString).toPromise()
-            .catch(VillainService.handleError);
+            .catch(VillainService.handleError)
     }
     edit(villain: Villain) {
-
         return this.http.put<Villain>(this.villainsUrl + '/' + villain.id, villain, {
             headers: new HttpHeaders().set('Content-Type', 'application/json')
         }).toPromise()
-            .catch(VillainService.handleError);
+            .catch(VillainService.handleError)
     }
 
     add(villain: Villain) {
         return this.http.put<Villain>(this.villainsUrl + '/' + villain.id, villain, {
             headers: new HttpHeaders().set('Content-Type', 'application/json')
         })
-        .toPromise()
-        .catch(VillainService.handleError);
+            .toPromise()
+            .catch(VillainService.handleError)
     }
 }
